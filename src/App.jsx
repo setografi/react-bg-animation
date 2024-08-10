9d4325157459848d55124d76917ec8c8e8f258ff
setografi <hitonaashbell@gmail.com>
2024-08-11 00:35:41 +0700

    Background Animation with React GLSL

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import ShadersMain from "./components/ShadersMain";
+
+function App() {
+  return (
+    <section className="w-full h-full">
+      <ShadersMain />
+    </section>
+  );
+}
+
+export default App;
